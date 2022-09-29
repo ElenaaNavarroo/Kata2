f71e54d55a7943ce378408e06ad3a1ebbd7ff281
Elena Navarro <99991881+ElenaaNavarroo@users.noreply.github.com>
2022-09-29 23:46:14 +0100

    Add files via upload
    Tercera versión de la kata 2

---
+++ src/kata2/Histogram.java
@@ -0,0 +1,21 @@
+package kata2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Histogram {
+    private final int[] data;
+    
+    public Histogram(int[] data){
+        this.data = data;
+    }
+
+    public Map<Integer, Integer> getHistogram() {
+        HashMap<Integer,Integer> histogram = new HashMap<>();
+        for(int i = 0; i < data.length; i++){
+            histogram.put(data[i], histogram.containsKey(data[i]) ? histogram.get(data[i]) + 1 : 1);
+        }
+        return histogram;
+    }
+}
+    
--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -1,19 +1,15 @@
-
 package kata2;
 
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class Kata2 {
 
     public static void main(String[] args) {
         int [] data = {4, 2, 6, 4, 9, 8, 0, 1, 2, 6, 3, 7, 8, 5, 6, 0};
-        Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
-        
-        for(int i = 0; i < data.length; i++){
-            histogram.put(data[i], histogram.containsKey(data[i]) ? histogram.get(data[i]) + 1 : 1);
-        }
+
+        Histogram histo = new Histogram(data);
+
+        Map<Integer,Integer> histogram = histo.getHistogram();
         
         for(Map.Entry<Integer, Integer> entry: histogram.entrySet()){
             System.out.println(entry.getKey() + " ==> " + entry.getValue());
